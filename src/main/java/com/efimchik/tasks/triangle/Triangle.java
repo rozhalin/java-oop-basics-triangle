52cd216439ae0265130651fc15d897341b36022d
thejerome <thejerome@mail.ru>
2020-03-20 15:22:09 +0300

    Point and Triangle skeleton

---
+++ src/main/java/com/efimchik/tasks/triangle/Triangle.java
@@ -0,0 +1,17 @@
+package com.efimchik.tasks.triangle;
+
+class Triangle {
+
+    public Triangle(Point a, Point b, Point c) {
+        //TODO
+    }
+
+    public double area() {
+        //TODO
+    }
+
+    public Point centroid(){
+        //TODO
+    }
+
+}
